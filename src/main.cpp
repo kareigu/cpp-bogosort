011a068ff550dab1b5b9dba59f4cea6d064e4262
Eetu <datmixor@gmail.com>
2020-04-23 07:45:14 +0300

    made an useless for loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <array>
+#include <algorithm>
+
+void printArr(int arrIndx) {
+    std::cout << arrIndx << std::endl;
+}
 
 int main() {
-  std::cout << "vittu paskaa" << std::endl;
-  return 0;
+    std::array<int, 9> sortedArr = {1,2,3,4,5,6,7,8,9};
+    std::array<int, 9> arrToSort = sortedArr;
+
+    for (int i = 0; i < arrToSort.size(); i++) {
+        printArr(arrToSort[i]);
+    }
+    return 0;
 }
