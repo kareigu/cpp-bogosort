de338cc1d5f37da7dadbd12e6b421bb22eee02eb
Eetu <datmixor@gmail.com>
2020-05-25 20:12:25 +0300

    added function for filling arrays with values

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,21 @@ void printArr(int arrIndx) {
     std::cout << arrIndx << std::endl;
 }
 
-int main() {
-    std::array<int, 9> sortedArr = {1,2,3,4,5,6,7,8,9};
-    std::array<int, 9> arrToSort = sortedArr;
+void fillArr(int arr [], int arrLen) {
+    for(int i = 0; i < arrLen; i++){
+        arr[i] = i + 1;
+    }
+}
 
-    for (int i = 0; i < arrToSort.size(); i++) {
+int main() {
+    int arrLen;
+    std::cin >> arrLen;
+    int sortedArr [arrLen];
+    fillArr(sortedArr, arrLen);
+    int arrToSort [arrLen];
+    fillArr(arrToSort, arrLen);
+
+    for (int i = 0; i < arrLen; i++) {
         printArr(arrToSort[i]);
     }
     return 0;
