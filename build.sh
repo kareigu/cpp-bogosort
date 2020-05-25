23c86f526a24041f78ae921287c9093899eed9dd
Eetu <datmixor@gmail.com>
2020-05-25 22:20:58 +0300

    updated build script to use C++17

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 CFILE=$1
 OBJFILE="${CFILE%.*}"
 
-g++ ./src/$CFILE -o ./bin/$OBJFILE
+g++ ./src/$CFILE -o ./bin/$OBJFILE -std=c++17
 echo  "Built file: ./src/$CFILE -> ./bin/$OBJFILE"
 
 ./bin/$OBJFILE
