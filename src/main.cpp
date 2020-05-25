4a9304a7eba478b5bdf6f6d35b850f2ca80978f4
Eetu <datmixor@gmail.com>
2020-05-25 22:21:23 +0300

    reworked array filling somehow

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,21 @@ void printArr(int arrIndx) {
     std::cout << arrIndx << std::endl;
 }
 
-void fillArr(int arr [], int arrLen) {
+int * fillArr(int arrLen) {
+    static int arr[10];
     for(int i = 0; i < arrLen; i++){
         arr[i] = i + 1;
     }
+    return arr;
 }
 
 int main() {
     int arrLen;
     std::cin >> arrLen;
-    int sortedArr [arrLen];
-    fillArr(sortedArr, arrLen);
-    int arrToSort [arrLen];
-    fillArr(arrToSort, arrLen);
+    const int *sortedArr = fillArr(arrLen);
+    //fillArr(sortedArr, arrLen);
+    int *arrToSort = fillArr(arrLen);
+    //fillArr(arrToSort, arrLen);
 
     for (int i = 0; i < arrLen; i++) {
         printArr(arrToSort[i]);
