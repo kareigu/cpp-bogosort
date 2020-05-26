56e5aa59d37c94deae7c2b36829ec4eed02101be
Eetu <datmixor@gmail.com>
2020-05-26 22:20:34 +0300

    Updated compiler to use C++20

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 CFILE=$1
 OBJFILE="${CFILE%.*}"
 
-g++ ./src/$CFILE -o ./bin/$OBJFILE -std=c++17
+g++ ./src/$CFILE -o ./bin/$OBJFILE -std=c++2a
 echo  "Built file: ./src/$CFILE -> ./bin/$OBJFILE"
 
 ./bin/$OBJFILE
