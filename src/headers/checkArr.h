601067b92f3e14603a58bc54abae323ad3253ecf
Eetu <datmixor@gmail.com>
2020-05-26 22:39:37 +0300

    Added basis for comparing values between arrays

---
+++ src/headers/checkArr.h
@@ -0,0 +1,6 @@
+bool checkArr(const int sArr[], int tArr[], int arrLen) {
+    for (int i = 0; i < arrLen; i++) {
+        if(sArr[i] != tArr[i]) {return false;}
+    }
+    return true;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "headers/printArr.h"
 #include "headers/fillArr.h"
 #include "headers/randArr.h"
+#include "headers/checkArr.h"
 
 int main() {
     int arrLen;
@@ -11,8 +12,18 @@ int main() {
     //fillArr(sortedArr, arrLen);
     int *arrToSort = fillArr(&arrLen);
     //fillArr(arrToSort, arrLen);
-
     randArr(arrToSort, arrLen);
+
+    bool isSorted = false;
+
+    while(true) {
+        if(checkArr(sortedArr, arrToSort, arrLen)){
+            isSorted = true;
+        }
+        else {
+            randArr(arrToSort, arrLen);
+        }
+    }
     for (int i = 0; i < arrLen; i++) {
         printArr(arrToSort[i]);
     }
