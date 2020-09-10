9fbfe3d5b401d9c9efdf4a062a1c1e7c3434a571
Eetu <eetu.kivelae@gmail.com>
2020-09-10 22:57:57 +0300

    Added xorshift based randomiser

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main() {
     logger::printArr(arr2, 5);
     logger::debug("val", 5);
 
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 10; i++) {
         logger::debug("Double", rng::genDouble());
         logger::debug("Int", rng::genInt());
     }
--- src/rng.cpp
+++ src/rng.cpp
@@ -5,6 +5,42 @@
 #include "../include/logger.hpp"
 
 namespace rng {
+  uint64_t rol64 (uint64_t x, int k) {
+    return (x << k) | (x >> (64 - k));
+  }
+
+  struct xoshiro256ss_state {
+	  uint64_t s[4];
+  };
+
+
+  uint64_t xoshiro256ss(struct xoshiro256ss_state *state) {
+    uint64_t *s = state->s;
+    uint64_t const result = rol64(s[1] * 5, 7) * 9;
+    uint64_t const t = s[1] << 17;
+
+    s[2] ^= s[0];
+    s[3] ^= s[1];
+    s[1] ^= s[2];
+    s[0] ^= s[3];
+
+    s[2] ^= t;
+    s[3] = rol64(s[3], 45);
+
+    return result;
+  }
+
+  uint64_t rollXos () {
+    struct xoshiro256ss_state state = {
+      (uint64_t) rand(),
+      (uint64_t) rand(),
+      (uint64_t) rand(),
+      (uint64_t) rand()
+    };
+    return xoshiro256ss(&state) / 1000000;
+  }
+
+
   void initRng () {
     srand((int) time(0));
     int seed = rand();
@@ -12,19 +48,25 @@ namespace rng {
     srand48(seed);
   }
 
-  int gen () {
-    return rand() % 1000;
-  }
-
   int genInt () {
-    return gen();
+    return rollXos();
   }
 
   double genDouble () {
     double *table = new double[10];
+    double floater = (double) (rand()) / (double) (RAND_MAX);
+    floater += 0.0001;
     for (int i = 0; i < 10; i++) {
-      table[i] = gen() * 0.33125 / 1.52134;
+      table[i] = rollXos() * floater;
     }
-    return table[rand() % 10];
+    int idx = rollXos() % 10;
+
+    if (idx > 9) {
+      return table[rand() % 10];
+      delete table;
+    } else {
+      return table[idx];
+      delete table;
+    }
   }
 }
